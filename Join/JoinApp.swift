ddb3a5ee797116bc3d0de024a9b9d3a7e9660e0e
Lerato Nteo <kagisano.nteo14@gmail.com>
2025-01-06 17:59:52 +0200

    Initial Commit

---
+++ Join/JoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoinApp.swift
+//  Join
+//
+//  Created by Limakatso Ntoba on 2025/01/06.
+//
+
+import SwiftUI
+
+@main
+struct JoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
